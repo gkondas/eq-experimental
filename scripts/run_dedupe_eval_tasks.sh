d3617519df8553e59bef7021b02e344dd2866f3e
Greg Kondas <gregkondas9@gmail.com>
2026-04-29 23:36:50 -0400

    update

--- scripts/run_dedupe_eval_tasks.sh
+++ scripts/run_dedupe_eval_tasks.sh
@@ -24,8 +24,8 @@ cd "${SLURM_SUBMIT_DIR:-$PWD}"
 mkdir -p logs
 
 # === Edit these for your run ===
-INPUT_DIR=/users/gbk2114/mimic-eq/eval_tasks/eval
-OUTPUT_DIR=/users/gbk2114/mimic-eq//eval_unique_index
+INPUT_DIR=/groups/mm6677_gp/gbk2114/columbia-meds/eval/eval
+OUTPUT_DIR=/groups/mm6677_gp/gbk2114/columbia-meds/eval_unique_index
 # ================================
 
 echo "Starting job on $(hostname) at $(date)"
