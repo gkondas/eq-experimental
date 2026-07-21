94a29d9f3a0d822214d6c774a426cbc7e09112b8
gkondas <gkondas@umich.edu>
2026-07-21 12:22:17 -0400

    up

--- eval_traj.py
+++ eval_traj.py
@@ -27,16 +27,16 @@ MEDS_PARQUET = Path(
     "/groups/mm6677_gp/data/MIMIC_MEDS/MEDS_cohort/intermediate/data/held_out"
 )  # reference MEDS dataframe
 TRAJ_ROOT = Path(
-    "/users/gbk2114/eic_stuff/trajectories_final_combined"
+    "/users/gbk2114/eic_stuff/rebuttle-traj-mimic/medium_seq_1024/combined"
 )  # dir OR single parquet file
 
 # yaml defining which codes/tasks to eval
 PREDICATES_YAML = Path(
-    "/users/gbk2114/mimic-eq/code-split/split__seed42__poolbbd2128d07ed__ood7450ad537df2__id1ad4b1f900eb/ar-eval-predicate-codes.yaml"
+    "/users/gbk2114/mimic-eq/code-split/split__seed42__poolbbd2128d07ed__ood7450ad537df2__id1ad4b1f900eb/ar-eval-predicate-codes-w-clinical.yaml"
 )
 
 OUT_PARQUET = Path(
-    "/users/gbk2114/eic_stuff/traj-eval-results/temporal-auc-results-final.parquet"
+    "/users/gbk2114/eic_stuff/mimic-rebut-traj-eval-results/temporal-auc-results-final.parquet"
 )
 
 # Optional knobs
